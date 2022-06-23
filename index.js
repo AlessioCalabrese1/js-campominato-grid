00f85f30d3934356c66013b567fe6bdb1239db55
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-06-23 16:36:04 +0200

    Bonus

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 let grillGeneratorButton = document.getElementById("grillGenerator");
 
 grillGeneratorButton.addEventListener("click", function() {
+    let difficultyOption = document.getElementById("difficulty").value;
+    console.log(difficultyOption);
+    let grillItemsNumber;
+    if (difficultyOption === "Difficulty 1") {
+        grillItemsNumber = 100;
+    } else if (difficultyOption === "Difficulty 2") {
+        grillItemsNumber = 81;
+    } else if (difficultyOption === "Difficulty 3") {
+        grillItemsNumber = 49;
+    }
+
+
     let grillContainer = document.getElementById("grillContainer");
 
-    for (let index = 0; index < 100; index++) {
+    for (let index = 0; index < grillItemsNumber; index++) {
         let grillItem = document.createElement("div");
-        grillItem.classList.add("grill-items-10", "d-inline-block", "bg-success", "gi-text");
+        if (difficultyOption === "Difficulty 1") {
+            grillItem.classList.add("grill-items-10", "d-inline-block", "bg-success", "gi-text");
+        } else if (difficultyOption === "Difficulty 2") {
+            grillItem.classList.add("grill-items-9", "d-inline-block", "bg-success", "gi-text");
+        } else if (difficultyOption === "Difficulty 3") {
+            grillItem.classList.add("grill-items-7", "d-inline-block", "bg-success", "gi-text");
+        }
+        
         grillItem.innerHTML = index + 1;
         grillItem.addEventListener("click", function() {
             console.log(grillItem.innerHTML);
